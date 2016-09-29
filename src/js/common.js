128e5c46b010b1e6c12452414a35e07683a6d25e
anatoliy5035 <senator5035@yandex.ru>
2016-09-30 02:59:07 +0300

    1day

---
+++ src/js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	// go js
+
+});
+
+// function setHeiHeight() {
+//     $('.wrapper').css({
+//         height: $(window).height() + 'px'
+//     });
+// }
+//
+// setHeiHeight();
+//
+// $(window).resize(setHeiHeight);
