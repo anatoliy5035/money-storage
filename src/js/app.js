7e87e40fd28189ddd1c25b03cacf07aa38769eb0
anatoliy5035 <senator5035@yandex.ru>
2016-10-08 11:24:05 +0300

    08.10

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 //=include lib/jquery.js
-//=include lib/slick.min.js
+//=include lib/scroll.js
 //=include common.js
--- src/js/common.js
+++ src/js/common.js
@@ -1,39 +1,16 @@
-$(document).ready(function() {
 
-	// go js
 
-});
 
-function setHeiHeight() {
-    $('.wrapper').css({
-        height: $(window).height() + 'px'
+//padding top
+    $(function() {
+        // if($(window).width()<767) {
+        $(' main').each(function () {
+            var headerHeight = $('.header').height();
+            $(this).css('padding-top', headerHeight + 'px');
+        });
+        // }
     });
-}
-setHeiHeight();
 
-$(window).resize(setHeiHeight);
-
-
-
-function setHeHeightBlock() {
-    $(window).height()
-}
-
-setHeHeightBlock();
-//
-$(function(){
-    $(' main').each(function(){
-        var headerHeight=$('.header').height();
-        $(this).css('padding-top',headerHeight+'px');
-    });
-});
-
-// $(function(){
-//     $('.list-of-currency').each(function(){
-//         var addrHeight=$('.add-ham').outerHeight();
-//         $(this).css('padding-bottom',addrHeight+'px');
-//     });
-// });
 
 
 $(window).resize(
@@ -43,4 +20,110 @@ $(window).resize(
             $(this).css('padding-top',headerHeight+'px');
         });
     }
+
 );
+
+$('.content-check input').change(function() {
+    if($(this).is(":checked")) {
+        $(this).parent().parent().addClass('active');
+        // $(this).attr("checked", returnVal);
+    }
+    else {
+        $(this).parent().parent().removeClass('active');
+    }
+});
+
+//scaling
+
+function fitBg() {
+    var maxWidth  = $('.phone').width();
+    var maxHeight = $('.phone').height();
+    var $window = $(window);
+    var width = $window.width();
+    var height = $window.height();
+    var scale;
+    console.log(scale);
+
+    // early exit
+    if(width >= maxWidth && height >= maxHeight) {
+        $('.image').css({'-webkit-transform': ''});
+        $('.wrap-bg').css({ width: '', height: '' });
+        return;
+    }
+
+    scale = Math.min(width/maxWidth, height/maxHeight);
+
+    $('.phone').css({'-webkit-transform': 'scale(' + scale + ')'});
+    $('.wrap-bg').css({  });
+}
+
+fitBg();
+
+$(window).resize(fitBg);
+
+
+//set table size
+function tableSize() {
+    var tablecellwidth = $('.bottom-panel td').outerWidth();
+    $('.bottom-panel td').css('height', tablecellwidth + 'px');
+};
+
+
+
+function setHeiHeight() {
+    $('.wrapper').css({
+        height: screen.height + 'px'
+    });
+}
+
+$('.list-of-currency').css({
+    height:  + 'px'
+});
+
+
+
+$(window).resize(setHeiHeight);
+
+
+function setHeighPanel() {
+    var footerHeight = $('.bottom-panel').height();
+    var headerHheight = $('.header').height();
+    var screenHeight = screen.height;
+    if($(window).width()<767) {
+        var calc = screenHeight - headerHheight - footerHeight;
+        $('.info-block-wrapp').css('height', calc + 'px');
+    }
+    else {
+        var calc = 629 - headerHheight - footerHeight;
+        $('.info-block-wrapp').css('height', calc + 'px');
+    }};
+
+
+$(window).resize(tableSize, setHeighPanel);
+
+tableSize();
+setHeiHeight();
+setHeighPanel();
+
+//iframe overflow
+// $('iframe').load( function() {
+//     $('iframe').contents().find("head")
+//         .append($("<style type='text/css'>.wrapper.without-panel {overflow: hidden !important; } .wrapper.with-panel { overflow: hidden !important;}  .wrapper{ height: 629px !important; overflow: scroll !important;}  </style>"));
+// });
+
+
+
+
+
+
+
+
+
+//scroll
+
+
+
+
+
+
+
